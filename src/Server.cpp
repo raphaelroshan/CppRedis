4494d260672718b4306742b44d76d3d63e3994da
raphaelroshan <49832307+raphaelroshan@users.noreply.github.com>
2024-03-14 16:48:45 +0800

    fixed error

--- src/Server.cpp
+++ src/Server.cpp
@@ -89,7 +89,7 @@ int main(int argc, char **argv) {
       int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
       std::cout << "Client connected\n";
       std::thread clientThread(handleClient, client_fd);
-      clientThread.detach()
+      clientThread.detach();
     }
   }
   
